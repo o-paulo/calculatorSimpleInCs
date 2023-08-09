996929bef814dd69a0e01b67d9d7566d810d9c56
Paulo Pereira | PROA.AI <paulo.pereira@proa.ai>
2023-08-09 00:04:52 -0300

    Calculadora simples em C#

---
+++ Aula08.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Aula07{
+    static void Main(){
+        int v1, v2, soma;
+        string nome;
+
+        Console.Write("Digite seu nome: ");
+        nome = Console.ReadLine();
+
+        Console.Write("{0} digite o primeiro valor: ", nome);
+        v1 = Convert.ToInt32(Console.ReadLine()); // Primeiro formato para converter um valor de string para int é usando Convert.ToInt32()
+
+        Console.Write("Digite o segundo valor: ");
+        v2 = int.Parse(Console.ReadLine()); // Segundo formato possivel é utilizando int.Parse()
+
+        soma = v1 + v2;
+
+        Console.Write("A soma entre {0} e {1} é: {2} ", v1, v2, soma);
+    }
+}
